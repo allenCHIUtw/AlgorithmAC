cc9151091581825b41984f0a59725dc6fcd35fdd
allenCHIUtw <allen@abin.com.tw>
2023-01-27 14:59:58 +0800

    allen

--- Algorithm_related/B09901036_pa2/src/mps_solver.cpp
+++ Algorithm_related/B09901036_pa2/src/mps_solver.cpp
@@ -2,12 +2,24 @@
 #include<iostream>
 #include<cstdlib>
 #include<fstream>
-MapCircle::MapCircle( int number, vector<chord>&){
-
+MapCircle::MapCircle( int number, vector<chord>& inputs){
+   
 }
 MapCircle::~MapCircle(){}
 
 void MapCircle::ADD(int aa)
 {
+
+}
+       
+void   MapCircle:: buildstate(states**checkstate ,int)
+{
+   
+}
+void   MapCircle:: findMIS(states**,int length ,int** MIS)
+{
+
+}
+void    MapCircle::findchords(int start , int end , int * , states ** checkstate, int**MIS ){
 
 }
--- Algorithm_related/B09901036_pa2/src/mps_solver.h
+++ Algorithm_related/B09901036_pa2/src/mps_solver.h
@@ -7,20 +7,28 @@ using namespace std;
 struct chord{
     int begin;
     int end;
-   
-
+};
+enum states{
+    CASE1 ,CASE2,CASE3,NA
 };
 class MapCircle{
    public:
                     MapCircle(int ,vector<chord>&); 
                     ~MapCircle();     
         void        ADD(int);
+        void        buildstate(states**,int);
+        void        findMIS(states**,int ,int**);
+        void        findchords(int , int , int *, states **, int** );
+
    
    private:
-   int size;
+   int size=0;
    int result=0;
    vector<chord> allchord;
-   vector<chord> result;
+   chord* result;
+   states **checkstate;
+   int **MIS;
+
   
 };
 
