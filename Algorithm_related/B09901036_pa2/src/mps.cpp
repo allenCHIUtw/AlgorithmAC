01ab1e15606b464c384c792aac7f1f4989b87f76
allenCHIUtw <allen@abin.com.tw>
2023-02-14 00:01:43 +0800

    me

--- Algorithm_related/B09901036_pa2/src/mps.cpp
+++ Algorithm_related/B09901036_pa2/src/mps.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdlib.h>
+#include<fstream>
 #include<vector>
 struct chord{
     int begin;
@@ -8,3 +9,33 @@ struct chord{
 enum states{
     CASE1 ,CASE2,CASE3,NA
 };
+using namespace std;
+int main()
+{   
+    fstream dtin; vector<int> data_string;
+    int planesize=data_string[0];
+    int csize=data_string.size()/2-1;
+    chord *ipchrd=new chord[csize];
+    for(int u=0; u<csize;  u++)
+    {
+         ipchrd[u].begin=0;
+
+         ipchrd[u].end=0;
+    }
+    int number ;
+    
+    int **MIS=new int*[planesize]; 
+    states **MIS_state = new states*[planesize];
+for (int i = 0; i < planesize; i++) {
+ MIS[planesize] = new int[planesize];
+ MIS_state[planesize] = new  states[planesize];
+}
+for (int i = 0; i < planesize; i++){
+    for (int ii = 0; ii < planesize; ii++)
+    {
+        MIS[i][ii]= 0 ;  MIS_state[i][ii] = NA;
+    }
+}
+
+    return 0;
+}
