0136709abe2622558c04358c24b739dcf210ec11
allenCHIUtw <allen@abin.com.tw>
2023-02-14 11:54:18 +0800

    me

--- Algorithm_related/B09901036_pa2/src/mps.cpp
+++ Algorithm_related/B09901036_pa2/src/mps.cpp
@@ -2,28 +2,25 @@
 #include<stdlib.h>
 #include<fstream>
 #include<vector>
-struct chord{
-    int begin;
-    int end;
-};
 enum states{
     CASE1 ,CASE2,CASE3,NA
 };
+int find_line(int*ipt,int key);
 using namespace std;
 int main()
 {   
     fstream dtin; vector<int> data_string;
     int planesize=data_string[0];
     int csize=data_string.size()/2-1;
-    chord *ipchrd=new chord[csize];
-    for(int u=0; u<csize;  u++)
-    {
-         ipchrd[u].begin=0;
-
-         ipchrd[u].end=0;
-    }
-    int number ;
-    
+    int *ipchrd=new int[planesize];
+    for(int u=0; u<planesize;  u++) {ipchrd[u]=-1;}
+    for(int ix=1; ix<data_string.size()-1; ++ix)
+      {
+        if(ix%2==0)
+        ipchrd[data_string[ix]]=data_string[ix+1];
+        else
+        ipchrd[data_string[ix]]=data_string[ix-1];
+      }
     int **MIS=new int*[planesize]; 
     states **MIS_state = new states*[planesize];
 for (int i = 0; i < planesize; i++) {
@@ -36,10 +33,13 @@ for (int i = 0; i < planesize; i++){
         MIS[i][ii]= 0 ;  MIS_state[i][ii] = NA;
     }
 }
-    for(int j=1; j<planesize; j++)
-    {
-
+    for(int j=0; j<planesize; j++)
+    {  
+     int strt=0; int ed=strt+j;
     }
 
     return 0;
+}
+int find_line(int *ipt,int key){
+   return ipt[key];
 }
